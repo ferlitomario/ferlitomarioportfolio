466b8572be1188ffa12ef3a24799112670037aa0
ferlitomario <ferlitomario99@gmail.com>
2023-02-28 22:39:42 +0100

    first commit

--- src/pages/project.js
+++ src/pages/project.js
@@ -7,9 +7,7 @@ import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 import image1 from '/src/images/site1.png';
 import image2 from '/src/images/background.jpg';
-
-
-
+import image3 from '/src/images/IA.png';
 
 
 const Layout = ({ children }) => (
@@ -18,10 +16,6 @@ const Layout = ({ children }) => (
     {children}
   </>
 )
-
-
-
-
 
 
 const projectsData = [
@@ -39,6 +33,20 @@ const projectsData = [
     description:"I have developed several projects for university courses in Linux using the c language or using the SHELL.",
     Url: "https://github.com/ferlitomario"
 
+  },
+  {
+    img: image3 ,
+    title: "OpenAI-API ",
+    description:"I created a chat-GPT clone using openAI's API.After entering the data in the prompt the image of the request will be displayed",
+    Url: "https://github.com/ferlitomario"
+
+  },
+  {
+    img: image2,
+    title: "Linux-OS Projects ",
+    description:"I have developed several projects for university courses in Linux using the c language or using the SHELL.",
+    Url: "https://github.com/ferlitomario"
+
   }
 ]
 
@@ -48,9 +56,10 @@ const SecondPage = () => (
   
   <Layout>
      <h1 style={{textAlign: "center",fontFamily:"BhuTuka Expanded One"}}>My Project</h1>
-     <div style={{height:'100%',width:'90%',display: "flex",alignitems:'center',margin:"3rem"}}>
-    {projectsData.map((data) => (
-      <Card sx={{ width: '40rem' , margin:"3rem",height:'300'}}>
+     <div className="card-container" style={{height:'50%',width:'90%',display: "grid",gridTemplateColumns: "repeat(auto-fit, minmax(35%, 1fr))",gap: "3rem", margin: "3rem"}}>
+     
+    {projectsData.map((data,index) => (
+      <Card key={index} sx={{  margin:"3rem",height:'200'}}>
         <CardActionArea>
           <CardMedia >
           <img src={data.img} alt="..." style={{ width: '100%',height:'100%',objectfit:'cover' }} />
